5f1083dc210a4507736d00fe6c47baf0d8477c3f
SAStommy <mingfungto@gmail.com>
2026-06-02 22:20:14 +0800

    v12

--- system/app.js
+++ system/app.js
@@ -270,27 +270,27 @@ const selfEfficacyQuestions = [
 
 const constructConfig = [
     {
-        name: "時間跨度",
-        left: "[具體]",
-        right: "[廣泛]",
+        name: "時間跨度(Time Span)",
+        left: "[具體](specific)",
+        right: "[廣泛](widely)",
         values: [3,3,3,3,3]//[1, 2, 4, 1, 5]
     },
     {
-        name: "空間範圍",
-        left: "[具體]",
-        right: "[廣泛]",
+        name: "空間範圍(Space Range)",
+        left: "[具體](specific)",
+        right: "[廣泛](widely)",
         values: [3,3,3,3,3]//[1, 3, 5, 4, 1]
     },
     {
-        name: "動作度強",
-        left: "[靜態]",
-        right: "[動態]",
+        name: "動作強度(Action Intensity)",
+        left: "[靜態](static)",
+        right: "[動態](dynamic)",
         values: [3,3,3,3,3]//[1, 1, 1, 5, 1]
     },
     {
-        name: "對象關連",
-        left: "[直接]",
-        right: "[潛在]",
+        name: "對象關連(Object Connection)",
+        left: "[直接](direct)",
+        right: "[潛在](latent)",
         values: [3,3,3,3,3]//[1, 1, 1, 4, 5]
     }
 ];
