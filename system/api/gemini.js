c097b1745a78c031edf0b28a097c9e721708e0ea
SAStommy <mingfungto@gmail.com>
2026-06-09 21:40:10 +0800

    v12

--- system/api/gemini.js
+++ system/api/gemini.js
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const client = new GoogleGenAI({
-    apiKey: process.env.JACK_GEMINI//GEMINI_API_KEY
+    apiKey: process.env.GEMINI_API_KEY
 });
 
 export default async function handler(req, res) {
